56756a28be3be8479a54307eb0192fe701d8bd16
sof_sepi <sven.depickere@sofico.be>
2020-06-12 08:26:58 +0200

    Update main.module.ts

--- src/app/main-module/main.module.ts
+++ src/app/main-module/main.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { MainViewComponent } from "./views/main-view/main-view.component";
+import { DetailViewComponent } from "./views/detail-view/detail-view.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
-    imports: [CommonModule],
-    declarations: [MainViewComponent],
+    imports: [CommonModule, RouterModule],
+    declarations: [MainViewComponent, DetailViewComponent],
     providers: [],
-    exports: [MainViewComponent]
+    exports: [MainViewComponent, DetailViewComponent]
 })
 export class MainModule {
 
