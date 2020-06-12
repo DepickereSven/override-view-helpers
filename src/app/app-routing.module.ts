b84100cb549717ef2d60e441941a005c68508630
sof_sepi <sven.depickere@sofico.be>
2020-06-12 08:26:49 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeViewComponent } from "./views/home-view/home-view.component";
 import { MainViewComponent } from "./main-module/views/main-view/main-view.component";
+import { DetailViewComponent } from "./main-module/views/detail-view/detail-view.component";
 
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeViewComponent
-  },
-  {
-    path: 'main-view',
-    component: MainViewComponent
-  }
+    {
+        path: '',
+        component: HomeViewComponent
+    },
+    {
+        path: 'main-view',
+        component: MainViewComponent
+    },
+    {
+        path: 'detail-view/:orderId',
+        component: DetailViewComponent
+    }
 ];
 
 @NgModule({
